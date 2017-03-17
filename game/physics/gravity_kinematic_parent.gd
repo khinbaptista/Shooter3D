1019a820c6f860ef6b6feb0577b83cc8ce4cb4c8
Khin Baptista <khin.baptista@gmail.com>
2017-03-17 00:47:09 -0300

    Added weight

--- game/physics/gravity_kinematic_parent.gd
+++ game/physics/gravity_kinematic_parent.gd
@@ -1,5 +1,7 @@
 extends Node
 
+export(float, 0, 5000, 0.1) var weight = 1
+
 export(bool) var override_gravity_speed = false
 export(bool) var override_gravity_vector = false
 
@@ -22,6 +24,6 @@ func _ready():
 
 func _fixed_process(delta):
 	if target.has_method("add_movement"):
-		target.add_movement(gravity_vector * gravity_speed)
+		target.add_movement(gravity_vector * gravity_speed * weight)
 	else:
-		target.move(gravity_vector * gravity_speed * delta)
+		target.move(gravity_vector * gravity_speed * weight * delta)
